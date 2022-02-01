4e129265fe576e1121e1eff567abe0517db9c81b
John Hanlon <john.hanlon@hginsights.com>
2022-02-01 15:10:54 -0800

    Implement testing

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -2,6 +2,24 @@ package org.hanlonjohn23;
 
 import org.scalatest.funsuite.AnyFunSuite
 
-public class SumTest {
-    test()
+class MainTest extends AnyFunSuite {
+    test("recursiveSum should return 0 if given an empty list") {
+        assert(Main.recursiveSum(List.empty) == 0)
+    }
+
+    test("recursiveSum should return value if given a single value") {
+        assert(Main.recursiveSum(Seq(1)) == 1)
+    }
+
+    test("recursiveSum should return a sum if given two values") {
+        assert(Main.recursiveSum(Seq(1,2)) == 3)
+    }
+
+    test("recursiveSum should return a sum if given a range of values") {
+        assert(Main.recursiveSum(Seq(1,2,3,4,5)) == 15)
+    }
+
+    test("recursiveSum should return a sum if given an set of values") {
+        assert(Main.recursiveSum(Seq(14,28,29,7,7,15)) == 100)
+    }
 }

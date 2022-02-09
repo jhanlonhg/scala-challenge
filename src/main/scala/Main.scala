c2730e9572974f07c43192af8661d52a6e2119a6
John Hanlon <john.hanlon@hginsights.com>
2022-02-09 15:14:51 -0800

    Upate Main to use new workflow

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,41 @@
 package org.hanlonjohn23
 import ArgParser.{Arguments, parser}
+import GeoResponseToLocationConverter._
 
-object Defaults {
-  val GEO_URL = "https://foo.bar"
-}
+import scala.math.{pow, sqrt}
+import org.hanlonjohn23.HttpRequester.GeocoderResponseBody
+import org.hanlonjohn23.LocationUtils.getClosestCities
+import scalaj.http._
+
+//case class CityLatLong
+
 
 object Main extends App {
+  case class Distance(
+                       location1: Location,
+                       location2: Location,
+                       length: Double
+                     )
+
+  def cityParser(cities: String): Seq[String] = {
+    // Split String on pipe character `|`
+    cities.split("\\|").map(_.trim)
+  }
+
+  def sendRequests(geoQuery: String) {
+    Http(Defaults.GEOCODER_URL).param("auth",Defaults.GEOCODER_AUTH).param("locate", geoQuery).param("json", "1").asString
+  }
+
   def run(arguments: Arguments): Unit = {
-    val cities: Seq[String] = cityParser(arguments)
+    val cities: Seq[String] = cityParser(arguments.cities)
 
-    val geoQuery: Json = buildQuery(cities)
+    val response: Seq[GeocoderResponseBody] = cities.map(HttpRequester.geoCoderGetRequest)
 
-    val response: Seq[Json] = sentRequests(geoQuery, Defaults.GEO_URL)
+    val citiesLatLong: Seq[Location] = response.map(_.toCityLatLong)
 
-    val citiesLatLong: Seq[CityLatLong] = getCityLatLongs(response)
+    val closestCities: Distance = getClosestCities(citiesLatLong)
 
-    val closestCities: (String, String) = getClosestCities(citiesLatLong)
-
-    print()
+    print(s"${closestCities.location1.city} and ${closestCities.location2.city} are closest")
   }
 
   parser.parse(args, Arguments()) match {

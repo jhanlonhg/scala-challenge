885608d542cb2349c5be6af77919cacb18f5f85f
John Hanlon <john.hanlon@hginsights.com>
2022-02-11 14:04:31 -0800

    Update Main to reflect changes elsewhere

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,14 @@
 package org.hanlonjohn23
 
 import ArgParser._
-import GeoResponseToLocationConverter._
-import HttpRequester.GeocoderResponseBody
 import LeastDistanceCalculator.getClosestCities
 
 object Main extends App {
   def run(arguments: Arguments): Unit = {
-    // Split passed argument on pipe character `|` & trim whitespace
-    val cities: Seq[String] = arguments.cities.split("\\|").map(_.trim)
-
-    // Ensure that at least two cities have been passed
-    cities match {
-      case Seq(single) => throw new IllegalArgumentException("Could not compare cities - Provide more than two cities")
-      case _ =>
-    }
+    val cities: Seq[String] = arguments.cities
 
     // Send Request to Geocoder service for passed args
-    val response: Seq[GeocoderResponseBody] = cities.map(HttpRequester.geoCoderGetRequest)
-
-    // Extract relevant location data from Geocoder response
-    val locations: Seq[Location] = response.map(_.toLocation)
+    val locations: Seq[Location] = cities.map(HttpRequester.geoCoderGetRequest)
 
     // Calculate which two cities are geographically closest
     val closestCities: Distance = getClosestCities(locations)

7e24679ec65c0c3e6aa8114cd0c4f2056d6bd8de
John Hanlon <john.hanlon@hginsights.com>
2022-02-22 15:27:41 -0800

    Move brewery specific distance code to Brewery file

--- src/main/scala/Brewery.scala
+++ src/main/scala/Brewery.scala
@@ -1,13 +1,13 @@
 package org.hanlonjohn23
 
+import LeastDistanceCalculator.{generateDistance, generateDistinctPairs}
+
 import io.circe.Decoder.Result
 import io.circe.{Decoder, HCursor}
 
 case class Brewery(
                     name: String,
-                    street: String,
-                    city: String,
-                    state: String,
+                    address: Address,
                     coordinates: Option[Coordinates],
                   )
 
@@ -18,30 +18,81 @@ case class ResolvedBrewery(
 
 
 object BreweryHelper {
-  implicit class CoordinatelessBrewery(brewery: Brewery) {
+  implicit class UnResolvedBrewery(brewery: Brewery) {
     def toResolvedBrewery: ResolvedBrewery = {
       ResolvedBrewery(
         name = brewery.name,
-        coordinates = brewery.coordinates.getOrElse{
-          HttpRequester.geocoderGetRequest(s"${brewery.street} ${brewery.city} ${brewery.state}")
+        coordinates = brewery.coordinates.getOrElse {
+          new GeoApi().getCoordinatesFor(brewery.address)
         }
       )
     }
+
+    def isResolvable: Boolean = {
+      (brewery.address.street, brewery.coordinates) match {
+        case ("", None) => false
+        case _ => true
+      }
+    }
+
+
   }
 
   val breweryDecoder: Decoder[Brewery] = new Decoder[Brewery] {
-    override final def apply(c: HCursor): Result[Brewery] = {
-      for {
-        name <- c.downField("name").as[String]
-        street <- c.downField("street").as[String]
-        city <- c.downField("city").as[String]
-        state <- c.downField("state").as[String]
-        latitude <- c.downField("latitude").as[Double]    // TODO: This decoding currently produces NaN instead of None
-        longitude <- c.downField("longitude").as[Double]  // TODO: This decoding currently produces NaN instead of None
-      } yield {
-        Brewery(name, street, city, state, Option(Coordinates(latitude, longitude)))
+    override final def apply(c: HCursor): Result[Brewery] = for {
+      name <- c.downField("name").as[String]
+      maybeStreet <- c.downField("street").as[Option[String]]
+      city <- c.downField("city").as[String]
+      state <- c.downField("state").as[String]
+      maybeLatitude <- c.downField("latitude").as[Option[Double]]
+      maybeLongitude <- c.downField("longitude").as[Option[Double]]
+    } yield {
+
+      val coordinates = (maybeLatitude, maybeLongitude) match {
+        case (Some(lat), Some(long))  => Some(Coordinates(lat, long))
+        case _                        => None
       }
+
+      val street = (maybeStreet) match {
+        case Some(street) => street
+        case None         => ""
+      }
+
+      Brewery(name, Address(street, city, state), coordinates)
     }
   }
 
+  def getClosestBreweries(breweries: Seq[ResolvedBrewery]): (ResolvedBrewery, ResolvedBrewery) = {
+    case class DistanceBetween(
+                                brewery1: ResolvedBrewery,
+                                brewery2: ResolvedBrewery,
+                                distance: Double
+                              ) {
+      def toBreweryTuple: (ResolvedBrewery, ResolvedBrewery) = {
+        (brewery1, brewery2)
+      }
+    }
+
+    def getMinBreweryDistance(between1: DistanceBetween, between2: DistanceBetween): DistanceBetween = {
+      if (between1.distance < between2.distance) {
+        between1
+      } else {
+        between2
+      }
+    }
+
+    implicit val distanceGenerator: DistanceGenerator = Haversine
+    // Generate unique pairs of all locations
+    generateDistinctPairs(breweries)
+      // Generate Distance objects using Location pairs
+      .map((breweryPair) => DistanceBetween(
+        breweryPair._1,
+        breweryPair._2,
+        generateDistance(breweryPair._1.coordinates, breweryPair._2.coordinates))
+      )
+      // Get Distance with smallest length
+      .reduce((x, y) => getMinBreweryDistance(x, y))
+      // Return tuple of Resolved Breweries
+      .toBreweryTuple
+  }
 }
--- src/main/scala/LeastDistanceCalculator.scala
+++ src/main/scala/LeastDistanceCalculator.scala
@@ -1,17 +1,9 @@
 package org.hanlonjohn23
 
 import scala.annotation.tailrec
-import scala.math.{pow, sqrt}
-import Haversine._
 
-private case class Distance(
-                             location1: Coordinates,
-                             location2: Coordinates,
-                             length: Double
-                   ) {
-  def toLocationTuple: (Coordinates, Coordinates) = {
-    (location1, location2)
-  }
+trait DistanceGenerator {
+  def generateDistance(locationPair: (Coordinates, Coordinates)): Double
 }
 
 object LeastDistanceCalculator {
@@ -27,44 +19,12 @@ object LeastDistanceCalculator {
     }
   }
 
-  def getClosestBreweries(breweries: Seq[Coordinates]): (Coordinates, Coordinates) = {
-    // Generate unique pairs of all locations
-    generateDistinctPairs(breweries)
-      // Generate Distance objects using Location pairs
-      .map(generateDistance)
-      // Get Distance with smallest length
-      .reduce((x,y) => getMinDistance(x, y))
-      .toLocationTuple
+  def generateDistance(locationPair: (Coordinates, Coordinates))(implicit distanceGenerator: DistanceGenerator): Double = {
+    distanceGenerator.generateDistance(locationPair)
   }
 
-  private def generateDistance(locationPair: (Coordinates, Coordinates)): Distance = {
-    val (location1, location2) = locationPair
-
-    // Calculate Haversine distance
-    val distanceBetween: Double = calculateHaversine(location1, location2)
-
-    Distance(
-      location1 = location1,
-      location2 = location2,
-      length = distanceBetween
-    )
-  }
-
-  private def generatePythagoreanDistance(locationPair: (Coordinates, Coordinates)): Distance = {
-    val (location1, location2) = locationPair
-
-    // Calculate Pythagorean distance
-    val distanceBetween: Double = sqrt(pow(location1.longitude - location2.longitude, 2) + pow(location1.latitude - location2.latitude, 2))
-
-    Distance(
-      location1 = location1,
-      location2 = location2,
-      length = distanceBetween
-    )
-  }
-
-  private def getMinDistance(distance1: Distance, distance2: Distance): Distance = {
-    if (distance1.length < distance2.length) {
+  private def getMinDistance(distance1: Double, distance2: Double): Double = {
+    if (distance1 < distance2) {
       distance1
     } else {
       distance2

076883840dd8c73a6cbfd70a2654982fbd028a47
John Hanlon <john.hanlon@hginsights.com>
2022-02-22 15:29:10 -0800

    Make classes that extend HttpRequester trait

--- src/main/scala/HttpRequester.scala
+++ src/main/scala/HttpRequester.scala
@@ -1,5 +1,5 @@
 package org.hanlonjohn23
 
 trait HttpRequester {
-  def get(params: Map[String, String]): String
+  def get(params: Seq[(String, String)]): String
 }
--- src/main/scala/ScalaJHttpRequester.scala
+++ src/main/scala/ScalaJHttpRequester.scala
@@ -3,15 +3,11 @@ package org.hanlonjohn23
 import scalaj.http.Http
 
 class ScalaJHttpRequester(url: String) extends HttpRequester {
-  override def get(params: Map[String, String]): String = {
-    val httpRequest = Http(url)
-
-    // Add params to Http
-    for ((param, value) <- params) httpRequest.param(param, value)
-
-    httpRequest
-      .asString   // Make request
+  override def get(params: Seq[(String, String)]): String = {
+    Http(url)
+      .params(params)
+      .asString // Make request
       .throwError // Throw error if applicable
-      .body       // Return body as String
+      .body // Return body as String
   }
 }

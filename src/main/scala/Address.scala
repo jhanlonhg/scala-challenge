862629be15ee86879c0629709fe88b303773f7e8
John Hanlon <john.hanlon@hginsights.com>
2022-02-23 13:36:50 -0800

    Refactor behavior to move GeoApi specific functions to GeoApi class

--- src/main/scala/Address.scala
+++ src/main/scala/Address.scala
@@ -1,20 +1,7 @@
 package org.hanlonjohn23
 
-object AddressDefaults {
-  val SUITE_REGEX = " (Ste|Apt|Unit) [a-zA-Z0-9]{1,4}"
-}
-
 case class Address(
                     street: String,
                     city: String,
                     state: String
-                  ) {
-  def getStreetAddress: String = {
-    s"${removeSubAddress(street)} $city, $state"
-  }
-
-  private def removeSubAddress(street: String): String = {
-    // Remove sub-address information (Ste, Apt, or Unit) as it causes issues with the Geocoder service
-    street.replaceAll(AddressDefaults.SUITE_REGEX, "")
-  }
-}
+                  )
--- src/main/scala/GeoApi.scala
+++ src/main/scala/GeoApi.scala
@@ -5,19 +5,32 @@ import io.circe.Decoder
 object GeoApiDefaults {
   val GEOCODER_URL = "https://geocoder.ca"
   val GEOCODER_AUTH: String = sys.env("GEOCODER_AUTH")
+  val SUBADDRESS_REGEX: String = " (Ste|Apt|Unit) [a-zA-Z0-9]{1,4}"
 }
 
 class GeoApi extends Api[Coordinates] {
-  override def httpRequester: HttpRequester = new ScalaJHttpRequester(GeoApiDefaults.GEOCODER_URL)
+  override val httpRequester: HttpRequester = new ScalaJHttpRequester(GeoApiDefaults.GEOCODER_URL)
 
   implicit val coordinateDecoder: Decoder[Coordinates] = CoordinatesHelper.coordinateDecoder
+
+  private implicit class GeoApiAddress(address: Address) {
+
+    def deriveStreetAddress: String = {
+      s"${address.removeSubAddress().street} ${address.city}, ${address.state}"
+    }
+
+    private def removeSubAddress(): Address = {
+      address.copy(street = address.street.replaceAll(AddressDefaults.SUBADDRESS_REGEX, ""))
+    }
+  }
 
   def getCoordinatesFor(address: Address): Coordinates = {
     val params: Seq[(String, String)] = Seq(
       ("auth", GeoApiDefaults.GEOCODER_AUTH),
-      ("locate", address.getStreetAddress),
+      ("locate", address.deriveStreetAddress),
       ("json", "1") // Request JSON response
     )
+
     getAndDecode(params)
   }
 }

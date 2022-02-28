01ed0ca96689648db6452b8c8cab08d7e05881bf
John Hanlon <john.hanlon@hginsights.com>
2022-02-28 15:31:29 -0800

    Extract Resolved Brewery to its own file

---
+++ src/main/scala/org/hanlonjohn23/models/ResolvedBrewery.scala
@@ -0,0 +1,6 @@
+package org.hanlonjohn23.models
+
+case class ResolvedBrewery(
+                            name: String,
+                            coordinates: Coordinates
+                          )

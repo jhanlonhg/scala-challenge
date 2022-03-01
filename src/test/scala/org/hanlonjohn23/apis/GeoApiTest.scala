30c50d10a0bcfc5ed470f90e72e067d4375e7f1c
John Hanlon <john.hanlon@hginsights.com>
2022-02-28 17:11:33 -0800

    Move Address tests to GeoApi tests to follow the unlying functionality. Tests are currently failing to build.

--- src/test/scala/org/hanlonjohn23/apis/GeoApiTest.scala
+++ src/test/scala/org/hanlonjohn23/apis/GeoApiTest.scala
@@ -1,30 +1,53 @@
 package org.hanlonjohn23.apis
 
-// TODO: Still not sure how I should be
+import org.hanlonjohn23.http.HttpRequester
+import org.hanlonjohn23.models.Address
+import org.scalatest.funsuite.AnyFunSuite
 
-//class MockHttpRequester(url: String) extends HttpRequester {
-//  override def get(params: Seq[(String, String)]): String = {
-//    val byCityParam = params.find(param => param._1 == "by_city").getOrElse(("by_city", "Wrong City"))
-//    s"""
-//       |[{
-//       |	"name": "The Foo Bar",
-//       |	"street": "1337 Baz Ave",
-//       |	"city": "${byCityParam._2}",
-//       |	"state": "California",
-//       |	"country": "United States",
-//       |	"longitude": null,
-//       |	"latitude": null
-//       |}]""".stripMargin
-//  }
-//}
-//
-//class GeoApiTest extends AnyFunSuite {
-//  // Initialize GeoApi for testing
-//  val geoApi = new GeoApi(new MockHttpRequester("http://notarealurl.com"))
-//
-//  // Fixtures
-//  val address = Address(street = ???, city = ???, state = ???)
-//
-//  test()
-//
-//}
+// TODO: Still not sure how I should be testing the HTTP functionality
+
+class MockHttpRequester(url: String) extends HttpRequester {
+  override def get(params: Seq[(String, String)]): String = {
+    val byCityParam = params.find(param => param._1 == "by_city").getOrElse(("by_city", "Wrong City"))
+    s"""
+       |[{
+       |	"name": "The Foo Bar",
+       |	"street": "1337 Baz Ave",
+       |	"city": "${byCityParam._2}",
+       |	"state": "California",
+       |	"country": "United States",
+       |	"longitude": null,
+       |	"latitude": null
+       |}]""".stripMargin
+  }
+}
+
+class GeoApiTest extends AnyFunSuite {
+  // Initialize GeoApi for testing
+  val geoApi = new GeoApi(new MockHttpRequester("http://notarealurl.com"))
+
+  // Fixtures
+  val cleanAddress: Address = Address(street = "1 N Calle Cesar Chavez", city = "Santa Barbara", state = "CA")
+  val addressWithSuite: Address = Address(street = "1 N Calle Cesar Chavez Ste 100", city = "Santa Barbara", state = "CA")
+  val addressWithUnit: Address = Address(street = "1 N Calle Cesar Chavez Unit A", city = "Santa Barbara", state = "CA")
+  val addressWithApt: Address = Address(street = "1 N Calle Cesar Chavez Apt A23B", city = "Santa Barbara", state = "CA")
+  val streetAddress: String = "1 N Calle Cesar Chavez Santa Barbara, CA"
+
+  test("An address object with no sub-address should serialize to itself") {
+    assert(geoApi.GeoApiAddress(cleanAddress).getStreetAddress == streetAddress)
+  }
+
+  test("An address object with a suite sub-address should serialize without the suite") {
+    assert(addressWithSuite.getStreetAddress == streetAddress)
+  }
+
+  test("An address object with a unit sub-address should serialize without the unit") {
+    assert(addressWithUnit.getStreetAddress == streetAddress)
+  }
+
+  test("An address object with an apartment sub-address should serialize without the apartment") {
+    assert(addressWithApt.getStreetAddress == streetAddress)
+  }
+
+
+}

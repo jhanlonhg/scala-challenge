da91537d9926ac2237dc6c62b50e7efcecc96a3f
John Hanlon <john.hanlon@hginsights.com>
2022-03-01 16:23:37 -0800

    Remove implicit class as it was creating more issues than it solved

--- src/main/scala/org/hanlonjohn23/apis/GeoApi.scala
+++ src/main/scala/org/hanlonjohn23/apis/GeoApi.scala
@@ -1,5 +1,6 @@
 package org.hanlonjohn23.apis
 
+import org.hanlonjohn23.apis.GeoApi.deriveStreetAddress
 import org.hanlonjohn23.http.{HttpRequester, ScalaJHttpRequester}
 import org.hanlonjohn23.models.{Address, Coordinates}
 
@@ -12,24 +13,23 @@ object GeoApiDefaults {
 class GeoApi(
               override val httpRequester: HttpRequester = new ScalaJHttpRequester(GeoApiDefaults.GEOCODER_URL)
             ) extends Api[Coordinates] {
-
-  protected implicit class GeoApiAddress(address: Address) {
-    def deriveStreetAddress: String = {
-      s"${address.removeSubAddress().street} ${address.city}, ${address.state}"
-    }
-
-    def removeSubAddress(): Address = {
-      address.copy(street = address.street.replaceAll(GeoApiDefaults.SUBADDRESS_REGEX, ""))
-    }
-  }
-
   def getCoordinatesFor(address: Address): Coordinates = {
     val params: Seq[(String, String)] = Seq(
       ("auth", GeoApiDefaults.GEOCODER_AUTH),
-      ("locate", address.deriveStreetAddress),
+      ("locate", deriveStreetAddress(address)),
       ("json", "1") // Request JSON response
     )
 
     getAndDecode(params)
+  }
+}
+
+object GeoApi {
+  private def deriveStreetAddress(address: Address): String = {
+    s"${removeSubAddress(address).street} ${address.city}, ${address.state}"
+  }
+
+  private def removeSubAddress(address: Address): Address = {
+    address.copy(street = address.street.replaceAll(GeoApiDefaults.SUBADDRESS_REGEX, ""))
   }
 }

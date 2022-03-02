4d4c8f7c5a7bc42515b72c506d7147132c3be6d6
John Hanlon <john.hanlon@hginsights.com>
2022-03-01 16:28:19 -0800

    Update API tests to use white box testing

--- src/test/scala/org/hanlonjohn23/apis/BrewApiTest.scala
+++ src/test/scala/org/hanlonjohn23/apis/BrewApiTest.scala
@@ -4,30 +4,29 @@ import org.hanlonjohn23.http.HttpRequester
 import org.hanlonjohn23.models.Address
 import org.scalatest.funsuite.AnyFunSuite
 
-class MockHttpRequester(url: String) extends HttpRequester {
-  override def get(params: Seq[(String, String)]): String = {
-    val byCityParam = params.find(param => param._1 == "by_city").getOrElse(("by_city", "Wrong City"))
-    s"""
-       |[{
-       |	"name": "The Foo Bar",
-       |	"street": "1337 Baz Ave",
-       |	"city": "${byCityParam._2}",
-       |	"state": "California",
-       |	"country": "United States",
-       |	"longitude": null,
-       |	"latitude": null
-       |}]""".stripMargin
-  }
-}
+
 
 class BrewApiTest extends AnyFunSuite {
-  // Initialize BrewApi for testing
-  val brewApi = new BrewApi(new MockHttpRequester("http://notarealurl.com"))
+  // Fixtures
+  def mockHttpRequester: HttpRequester = (params: Seq[(String, String)]) => {
+      val byCityParam = params.find(_._1 == "by_city")
+      assert(byCityParam.contains(("by_city", "Anytown")))
+      s"""
+         |[{
+         |	"name": "The Foo Bar",
+         |	"street": "1337 Baz Ave",
+         |	"city": "${byCityParam.getOrElse(("by_city", "Wrong City"))._2}",
+         |	"state": "California",
+         |	"country": "United States",
+         |	"longitude": null,
+         |	"latitude": null
+         |}]""".stripMargin
+  }
 
   test("findBreweriesIn should return results from that city") {
-    val breweries = brewApi.findBreweriesIn("Anytown")
+    val response = new BrewApi(mockHttpRequester).findBreweriesIn("Anytown")
 
-    val address = breweries.head.address
+    val address = response.head.address
 
     assert(address.contains(Address("1337 Baz Ave", "Anytown", "California")))
   }
--- src/test/scala/org/hanlonjohn23/apis/GeoApiTest.scala
+++ src/test/scala/org/hanlonjohn23/apis/GeoApiTest.scala
@@ -1,53 +1,64 @@
 package org.hanlonjohn23.apis
 
 import org.hanlonjohn23.http.HttpRequester
-import org.hanlonjohn23.models.Address
+import org.hanlonjohn23.models.{Address, Coordinates}
 import org.scalatest.funsuite.AnyFunSuite
 
-// TODO: Still not sure how I should be testing the HTTP functionality
-
-class MockHttpRequester(url: String) extends HttpRequester {
-  override def get(params: Seq[(String, String)]): String = {
-    val byCityParam = params.find(param => param._1 == "by_city").getOrElse(("by_city", "Wrong City"))
-    s"""
-       |[{
-       |	"name": "The Foo Bar",
-       |	"street": "1337 Baz Ave",
-       |	"city": "${byCityParam._2}",
-       |	"state": "California",
-       |	"country": "United States",
-       |	"longitude": null,
-       |	"latitude": null
-       |}]""".stripMargin
-  }
-}
-
 class GeoApiTest extends AnyFunSuite {
-  // Initialize GeoApi for testing
-  val geoApi = new GeoApi(new MockHttpRequester("http://notarealurl.com"))
-
   // Fixtures
   val cleanAddress: Address = Address(street = "1 N Calle Cesar Chavez", city = "Santa Barbara", state = "CA")
   val addressWithSuite: Address = Address(street = "1 N Calle Cesar Chavez Ste 100", city = "Santa Barbara", state = "CA")
   val addressWithUnit: Address = Address(street = "1 N Calle Cesar Chavez Unit A", city = "Santa Barbara", state = "CA")
   val addressWithApt: Address = Address(street = "1 N Calle Cesar Chavez Apt A23B", city = "Santa Barbara", state = "CA")
+
   val streetAddress: String = "1 N Calle Cesar Chavez Santa Barbara, CA"
+  val coordinates: Coordinates = Coordinates(34.418822, -119.683188)
+  val apiResponse: String =
+    """
+      |{
+      |	"standard": {
+      |		"staddress": {},
+      |		"stnumber": {},
+      |		"prov": "CA",
+      |		"city": "Santa Barbara",
+      |		"postal": {},
+      |		"confidence": "0.8"
+      |	},
+      |	"longt": "-119.683188",
+      |	"latt": "34.418822"
+      |}
+      |""".stripMargin
 
-  test("An address object with no sub-address should serialize to itself") {
-    assert(geoApi.GeoApiAddress(cleanAddress).getStreetAddress == streetAddress)
+  def mockHttpRequester: HttpRequester = (params: Seq[(String, String)]) => {
+    assert(params.find(_._1 == "locate").contains(("locate", streetAddress)))
+    apiResponse
   }
 
-  test("An address object with a suite sub-address should serialize without the suite") {
-    assert(addressWithSuite.getStreetAddress == streetAddress)
+  test("getAndDecode should send a clean street address to Geocoder.ca when provided a clean Address") {
+    // White box test GeoApi functionality as it interacts with HttpRequester
+    val response = new GeoApi(mockHttpRequester).getCoordinatesFor(cleanAddress)
+
+    assert(response == coordinates)
   }
 
-  test("An address object with a unit sub-address should serialize without the unit") {
-    assert(addressWithUnit.getStreetAddress == streetAddress)
+  test("getAndDecode should send a clean street address to Geocoder.ca when provided an Address with a suite") {
+    // White box test GeoApi functionality as it interacts with HttpRequester
+    val response = new GeoApi(mockHttpRequester).getCoordinatesFor(addressWithSuite)
+
+    assert(response == coordinates)
   }
 
-  test("An address object with an apartment sub-address should serialize without the apartment") {
-    assert(addressWithApt.getStreetAddress == streetAddress)
+  test("getAndDecode should send a clean street address to Geocoder.ca when provided an Address with an apartment") {
+    // White box test GeoApi functionality as it interacts with HttpRequester
+    val response = new GeoApi(mockHttpRequester).getCoordinatesFor(addressWithApt)
+
+    assert(response == coordinates)
   }
 
+  test("getAndDecode should send a clean street address to Geocoder.ca when provided an Address with a unit") {
+    // White box test GeoApi functionality as it interacts with HttpRequester
+    val response = new GeoApi(mockHttpRequester).getCoordinatesFor(addressWithUnit)
 
+    assert(response == coordinates)
+  }
 }
